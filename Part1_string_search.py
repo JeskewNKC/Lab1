1ba91a3f47655bfed50fe4f2edc2a403b5f046db
JeskewNKC <32969486+JeskewNKC@users.noreply.github.com>
2018-09-18 18:48:22 -0500

    Update Part1_string_search.py

--- Part1_string_search.py
+++ Part1_string_search.py
@@ -1,7 +1,10 @@
-import collectx2 = x2.lower()ions
-def string_find(x):
-    x2 = x.replace(' ','')
 
+import collections
+
+def string_find(x):
+
+    x2 = x.replace(' ','')
+    x2 = x2.lower()
     c = collections.Counter(x2)
     y = [char for char in x2 if c[char] ==1]
     print (x)
